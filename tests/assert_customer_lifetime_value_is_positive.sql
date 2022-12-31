bb622a3080899846755b948ca5c0db01b4d5d2af
Biruk Getaneh <bkgetmom@gmail.com>
2022-12-31 16:10:18 +0300

    test customer lifetime payment value if postive

---
+++ tests/assert_customer_lifetime_value_is_positive.sql
@@ -0,0 +1,11 @@
+-- Refunds have a negative amount, so the total amount should always be >= 0.
+-- Therefore return records where this isn't true to make the test fail
+SELECT
+    customer_id,
+    Sum(customer_lifetime_value) as order_amount
+
+FROM {{ ref('dim_customers' )}}
+
+GROUP BY 1
+
+HAVING NOT(SUM(customer_lifetime_value) >= 0)
